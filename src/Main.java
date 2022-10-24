9c4cede557edd5cfff55f0c6f0539a64078e4984
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-10-24 01:04:59 -0400

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Lasagna lasagna = new Lasagna();
         System.out.println(lasagna.ExpectedMinutesInOven());
+        System.out.println(lasagna.RemainingMinutesInOven(30));
+        System.out.println(lasagna.PreparationTimeInMinutes(2));
+        System.out.println(lasagna.ElapsedTimeInMinutes(3,20));
     }
 }
