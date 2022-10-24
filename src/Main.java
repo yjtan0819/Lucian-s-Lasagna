e88e421f2c265d1b0520a432feca0a7d1a6f154a
Yi Jia Tan <tan.yijia0819@gmail.com>
2022-10-24 01:11:47 -0400

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,11 @@ public class Main {
         System.out.println(lasagna.RemainingMinutesInOven(30));
         System.out.println(lasagna.PreparationTimeInMinutes(2));
         System.out.println(lasagna.ElapsedTimeInMinutes(3,20));
+
+        Lasagna lasagna2 = new Lasagna(60,5);
+        System.out.println(lasagna2.ExpectedMinutesInOven());
+        System.out.println(lasagna2.RemainingMinutesInOven(20));
+        System.out.println(lasagna2.PreparationTimeInMinutes(4));
+        System.out.println(lasagna2.ElapsedTimeInMinutes(4,30));
     }
 }
